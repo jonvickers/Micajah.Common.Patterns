ab6def1c476ac9bc5ebef7a76bcacb09af22b1a8
Jon Vickers <jon.vickers@micajah.com>
2011-10-12 15:22:26 -0400

    error tracking adjustments

---
+++ error/generate.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MCPatterns.error
+{
+    public partial class generate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int result = 100 / int.Parse("0");
+
+
+        }
+    }
+}
